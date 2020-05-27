b83898d6644e7816b7fe7c7fcc74ed215671d262
Renchiiks <60191731+Renchiiks@users.noreply.github.com>
2020-05-27 16:50:22 +0300

    Delete Constants.java

--- TourismWebApi/src/main/java/com/example/demo/data/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.data;
-
-public class Constants {
-    public static final String SCHEMA_NAME = "bootcamp_final_project";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD= "Nate2763666";
-}
